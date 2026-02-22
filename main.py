2aa2a0bc7bcba7e1b5eff69e422929fd4db367c8
TrashPig360 <nvardin@icloud.com>
2026-02-22 10:19:41 -0500

    mvp python code

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import time
+import senderTest
+import requests
+ESP32_IP = "172.20.10.3"
+ESP32_URL = f"http://{ESP32_IP}/instruments"
 
+
+start = time.perf_counter()
+instruments = ""
 def doEsp32():
-    pass
+    global start, instruments
+    now = time.perf_counter()
+    if now-start >  1:
+        start = time.perf_counter()
+        newInstruments = getInstruments()
+        if newInstruments != instruments:
+            instruments = newInstruments
+            print("starting new music with:", instruments)
+            prompt = f"make an upbeat song sure to excite people with only these instruments: {instruments}"
+            senderTest.generate_and_play_music(prompt, 20)
+
+
+def getInstruments():
+    try:
+        response = requests.get(ESP32_URL, timeout=2)
+
+        if response.status_code == 200:
+            return response.text.strip()
+
+    except requests.RequestException as e:
+        print("ESP32 request failed:", e)
+
+    return None
 
 def doCV():
     pass
