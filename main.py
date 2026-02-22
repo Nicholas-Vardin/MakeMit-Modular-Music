fe98dc1ac88181ec593b17bcdea0c08aa08a81ae
TrashPig360 <nvardin@icloud.com>
2026-02-22 01:25:13 -0500

    added loop

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+def doEsp32():
+    pass
+
+def doCV():
+    pass
+
+def main():
+    running = True
+    while(running):
+        doEsp32()
+        doCV()
+
+if __name__ == "__main__":
+    main()
